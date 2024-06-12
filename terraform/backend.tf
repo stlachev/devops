77d256d4c786fe7aef00059248bd4d3a44ae3eba
stlachev <stoyan.lachev@bosch.com>
2024-06-12 14:38:13 +0300

    region added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "gpm-tfstatestorage-s3"
     key    = "global/s3/terraform.tfstate"
-    region = var.aws_region_name
+    region = "eu-central-1"
     encrypt=true
   }
 }
