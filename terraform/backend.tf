1b1ee77099d7910a68affb1825ab354809c50b25
stlachev <stoyan.lachev@bosch.com>
2024-06-13 13:06:06 +0300

    s3 not used

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-bucket-2323"
-    key            = "terraform/terraform.tfstate"
+    key    = "terraform/terraform.tfstate"
 //    key = "main"
     region = "eu-central-1"
 //    dynamodb_table = "my-dynamo-db-table-tf"
