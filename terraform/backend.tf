bb36474eda4906145ff33b568c1c8ebce912ae7a
stlachev <stoyan.lachev@bosch.com>
2024-06-13 13:12:49 +0300

    dynamodb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-bucket-2323"
     key    = "terraform/terraform.tfstate"
-//    key = "main"
     region = "eu-central-1"
-//    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamo-db-table-tf"
   }
 }
