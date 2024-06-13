48532b7a73eb2b7867beb033e07e3f5f74aaad10
stlachev <stoyan.lachev@bosch.com>
2024-06-13 12:26:19 +0300

    bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "my-terraform-s3-bucket-23" {
+  bucket  = "my-bucket-2323"
+  tags    = {
+    Name           = "MyS3Bucket"
+    Environment    = "Production"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my-terraform-s3-bucket-23-acl" {
+  bucket = aws_s3_bucket.my-terraform-s3-bucket-23.id
+  acl    = "private"
+}
